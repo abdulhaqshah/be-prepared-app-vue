c1ca102e06f1ae3bd85c7e69caedaafeee22436d
abdulhaqshahg <abdulhaq.shah@southvillesol.com>
2019-05-16 15:12:22 +0500

    code cleaning and css restructuring

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
 <template>
-  <div id="app">
-    <Home></Home>
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
-    <!-- <router-view /> -->
-  </div>
+  <Home></Home>
 </template>
 
 <script>
@@ -17,37 +10,3 @@ export default {
   }
 }
 </script>
-
-
-<style>
-body {
-  background-color: #f5f5f5;
-  margin: 0;
-  padding: 0;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-    sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
